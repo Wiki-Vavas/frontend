44279d748f18593b66823acc38b93bd008185f8a
sads4ndwich <caiocamargo007@gmail.com>
2024-06-10 20:04:13 -0300

    feat: finish select map page AB#17

--- src/pages/maps.tsx
+++ src/pages/maps.tsx
@@ -1,5 +1,15 @@
+import { cn } from "../utils/cn";
+
 import Footer from "../components/Footer";
 import Nav from "../components/Nav";
+
+const maps = [
+  { label: "Split", image: "/imgs/maps/split.png" },
+  { label: "Heaven", image: "/imgs/maps/heaven.png" },
+  { label: "Bind", image: "/imgs/maps/bind.png"  },
+  { label: "Icebox", image: "/imgs/maps/icebox.png"  },
+  { label: "Pearl", image: "/imgs/maps/pearl.png"  },
+]
 
 export default function Maps() {
     return (
@@ -7,27 +17,18 @@ export default function Maps() {
             <Nav />
 
             <main className="px-44 py-12 bg-[#111723]">
-                <ul className="flex justify-center items-center -space-x-28">
-                    <li className="relative w-fit bg-red group hover:z-40 hover:scale-110 transition-all">
-                        <img src="/imgs/bind.png" alt="bind map" className="w-[90%] group-hover:w-full" />
-                        <p className="absolute bottom-0 left-1/2 -translate-x-1/2 text-5xl uppercase font-bold text-[#D5224E] mb-10">Split</p>
-                    </li>
-                    <li className="relative w-fit bg-red z-20 group hover:z-40 hover:scale-110 transition-all">
-                        <img src="/imgs/bind.png" alt="bind map" className="w-[95%] group-hover:w-full" />
-                        <p className="absolute bottom-0 left-1/2 -translate-x-1/2 text-5xl uppercase font-bold text-[#D5224E] mb-10">Split</p>
-                    </li>
-                    <li className="relative w-fit bg-red z-30 group hover:scale-110 transition-all">
-                        <img src="/imgs/bind.png" alt="bind map" />
-                        <p className="absolute bottom-0 left-1/2 -translate-x-1/2 text-5xl uppercase font-bold text-[#D5224E] mb-10">Split</p>
-                    </li>
-                    <li className="relative w-fit bg-red z-20 group hover:z-40 hover:scale-110 transition-all">
-                        <img src="/imgs/bind.png" alt="bind map" className="w-[95%] group-hover:w-full" />
-                        <p className="absolute bottom-0 left-1/2 -translate-x-1/2 text-5xl uppercase font-bold text-[#D5224E] mb-10">Split</p>
-                    </li>
-                    <li className="relative w-fit bg-red group hover:z-40 hover:scale-110 transition-all">
-                        <img src="/imgs/bind.png" alt="bind map" className="w-[90%] group-hover:w-full" />
-                        <p className="absolute bottom-0 left-1/2 -translate-x-1/2 text-5xl uppercase font-bold text-[#D5224E] mb-10">Split</p>
-                    </li>
+                <ul className="flex justify-center items-center -space-x-20">
+                    {maps.map((map, i) => (
+                        <li
+                            key={map.label}
+                            className={cn("relative w-fit bg-red group hover:z-40 hover:scale-110 transition-all", (i === 1 || i === 3) && "z-20", i === 2 && "z-30")}
+                        >
+                            <img src={map.image} alt="bind map" />
+                            <p className="absolute bottom-0 left-1/2 -translate-x-1/2 text-5xl uppercase font-bold text-[#D5224E] mb-10">
+                              {map.label}
+                            </p>
+                        </li>
+                    ))}
                 </ul>
             </main>
 
